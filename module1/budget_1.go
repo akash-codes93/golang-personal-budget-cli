5e63737dfb1a618910fb782cebb4a3ea4256ad37
Akash Gupta <akash.gupta1@olx.com>
2023-01-15 21:17:07 +0530

    module 1

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,13 +1,13 @@
 package module1
 
 // Budget stores budget information
-type struct Budget {
- Max   float32
- Items Item[]
+type Budget struct {
+	Max   float32
+	Items []Item
 }
 
 // Item stores item information
-type struct Item {
- Description string
- Price       float32
+type Item struct {
+	Description string
+	Price       float32
 }
